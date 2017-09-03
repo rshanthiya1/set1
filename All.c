1591fafcd832782caa8bd0e1c25447cdb999baf9
rshanthiya1 <31534986+rshanthiya1@users.noreply.github.com>
2017-09-03 06:40:42 +0530

    Create All.c

---
+++ All.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+Printf("enter the charecter");
+if(ch>='a')&&(ch<='z')
+{
+pritf("the char is alphabet");
+}
+else
+{
+printf("the char is not alphabet");
+}
+}
